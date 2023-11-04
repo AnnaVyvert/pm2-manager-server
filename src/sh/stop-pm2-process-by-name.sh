ff92a7e0b1d0091679fb405139a025ff57666435
Nikita Sakovich <n.sakovich@ispsystem.com>
2023-11-04 20:05:22 +0800

    feat: 🎸 add check on process existing

--- src/sh/stop-pm2-process-by-name.sh
+++ src/sh/stop-pm2-process-by-name.sh
@@ -4,6 +4,11 @@ source $1
 BE_PROCESS_ID=$(pm2 status | grep "$RFHSCR_BE_PROCESS_NAME" | awk '{print $2}')
 FE_PROCESS_ID=$(pm2 status | grep "$RFHSCR_FE_PROCESS_NAME" | awk '{print $2}')
 
+if [[ -z "$BE_PROCESS_ID" || -z "$FE_PROCESS_ID" ]]; then
+  echo "no processes with such names"
+  exit 1
+fi
+
 pm2 stop $BE_PROCESS_ID $FE_PROCESS_ID
 
 echo "YES"
